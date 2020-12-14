377a95fa1bd23a8894147cb22a5563e6c0c998fd
bambi <sarnina@me.com>
2020-12-14 20:35:52 +0100

    Image ratio algorithm done

--- assets/js/veneer.js
+++ assets/js/veneer.js
@@ -1,5 +1,6 @@
 var veneer = document.getElementById('veneer');
 var veneerImage = document.getElementsByClassName('veneer-image')[0];
+var veneerImageContainer = document.getElementsByClassName('veneer-image-container')[0];
 var veneerCloseButton = document.getElementById('veneer-close-button');
 var veneerGallery = document.getElementById('veneer-gallery');
 var veneerGalleryImages = document.getElementsByClassName('veneer-gallery-image');
@@ -10,12 +11,34 @@ var veneerNavNext = document.getElementById('veneer-right-area');
 var imagesSrc = [];
 var imagesAlt = [];
 
-
 Array.from(veneerGalleryImages).forEach(element => {
   imagesSrc.push(element.src)
   imagesAlt.push(element.alt)
 
 });
+
+
+// SET IMAGE CONTAINERS WIDTH 
+
+function veneerSetContainerWidth() {
+  if (veneerImage.naturalHeight > veneerImageContainer.offsetHeight) {
+
+    var heightDifference = veneerImage.naturalHeight - veneerImageContainer.offsetHeight
+    var percentage = heightDifference / veneerImage.naturalHeight * 100
+    var containerWidth = veneerImage.naturalWidth - (veneerImage.naturalWidth / 100 * percentage)
+    veneerImageContainer.style.width = containerWidth + 'px';
+    console.log('container width = ', containerWidth)
+  }
+}
+
+function veneerResetContainerWidth() {
+  veneerImageContainer.style.width = 'unset';
+}
+
+window.addEventListener('resize', function (event) {
+  veneerResetContainerWidth()
+  veneerSetContainerWidth()
+})
 
 
 // OPEN VENEER 
@@ -28,6 +51,7 @@ veneerGallery.addEventListener('click', function (event) {
     veneerImage.src = event.target.src
     veneerImage.alt = event.target.alt
     openVeneer()
+    veneerSetContainerWidth()
   }
 });
 
@@ -35,9 +59,11 @@ veneerGallery.addEventListener('click', function (event) {
 
 // CLOSE VENEER 
 function closeVeneer() {
-  veneer.classList.remove('veneer-on')
-  veneerImage.src = ""
-  veneerImage.alt = ""
+  veneer.classList.remove('veneer-on');
+  veneerImage.src = "";
+  veneerImage.alt = "";
+  veneerResetContainerWidth()
+
 }
 
 
@@ -46,36 +72,40 @@ veneerCloseButton.addEventListener('click', function () {
 })
 
 veneer.addEventListener('click', function (event) {
-  var clickInside = veneerImage.contains(event.target)
-
+  var clickInside = veneerImageContainer.contains(event.target)
   if (!clickInside) {
     closeVeneer()
   }
 })
 
 // VENEER NAVIGATION 
-var currentImage = imagesSrc[0]
 
-var imageIndex = parseInt(imagesSrc.indexOf(currentImage));
-
-function nextImage() {
-
+function veneerNextImage() {
+  var imageIndex = parseInt(imagesSrc.indexOf(veneerImage.src));
   if (imageIndex < (imagesSrc.length - 1)) {
     veneerImage.src = imagesSrc[imageIndex += 1];
+    veneerImage.alt = imagesAlt[imageIndex];
+
+    veneerSetContainerWidth();
   }
 }
 
-function previousImage() {
-  if (imageIndex >= 0) {
+function veneerPreviousImage() {
+  var imageIndex = parseInt(imagesSrc.indexOf(veneerImage.src));
+
+  if (imageIndex > 0) {
     veneerImage.src = imagesSrc[imageIndex -= 1];
+    veneerSetContainerWidth();
   }
 };
 
 veneerNavNext.addEventListener('click', function (event) {
-  nextImage();
+  veneerResetContainerWidth()
+  veneerNextImage();
 })
 
 veneerNavPrevious.addEventListener('click', function (event) {
-  previousImage();
+  veneerResetContainerWidth()
+  veneerPreviousImage();
 })
 
